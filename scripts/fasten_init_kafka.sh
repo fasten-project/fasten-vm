f47f928b3770dec1613fd76088e90f664667c3b1
Georgios Gousios <gousiosg@gmail.com>
2020-09-03 11:09:48 +0200

    Fix topic initialization

--- scripts/fasten_init_kafka.sh
+++ scripts/fasten_init_kafka.sh
@@ -7,4 +7,4 @@ for topic in "${topics[@]}"; do
   kafka-topics.sh --create --zookeeper localhost:2181 --partitions 9 --replication-factor 1 --topic "fasten.$topic.err"
 done
 
-kafka-topics.sh --create --zookeeper localhost:2181 --topic fasten.RapidPlugin.log
+kafka-topics.sh --create --zookeeper localhost:2181 --partitions 9 --replication-factor 1 -topic fasten.RapidPlugin.log
