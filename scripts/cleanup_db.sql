e146ba1de200991bb4b113aa72693380f079e900
fasten <fasten@example.com>
2020-09-03 15:28:16 +0200

    update db scripts

--- scripts/cleanup_db.sql
+++ scripts/cleanup_db.sql
@@ -1,13 +1,2 @@
 DROP DATABASE fasten_java;
-DROP DATABASE fasten_c;
-DROP DATABASE fasten_rust;
-DROP DATABASE fasten_python;
-
-DROP OWNED BY fasten;
 DROP USER fasten;
-
-DROP OWNED BY fastenro;
-DROP USER fastenro;
-
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
