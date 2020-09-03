ed088858bdefc3965dcaed8720997c2a5536ffa9
fasten <example@myemail.com>
2020-09-03 10:16:48 +0200

    Add new changes to scripts and .bash_profile

--- scripts/fasten_init_kafka.sh
+++ scripts/fasten_init_kafka.sh
@@ -1,10 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-source /home/fasten/scripts/kafkatopics.sh
+topics=(GraphDBExtension MetadataDBExtension OPAL POMAnalyzer RepoCloner pypi_coords pycg RapidPlugin)
 
 for topic in "${topics[@]}"; do
-  kafka-topics.sh --create --zookeeper localhost:2181 --partitions 9 --replication-factor 1 --topic "fasten.$topic.out"
-  kafka-topics.sh --create --zookeeper localhost:2181 --partitions 9 --replication-factor 1 --topic "fasten.$topic.err"
+  kafka-topics.sh --zookeeper localhost:2181 --delete --topic "fasten.$topic.out"
+  kafka-topics.sh --zookeeper localhost:2181 --delete --topic "fasten.$topic.err"
+done
+
+kafka-topics.sh --zookeeper localhost:2181 --delete --topic fasten.RapidPlugin.log
+
+for topic in "${topics[@]}"; do
+  kafka-topics.sh --create --zookeeper localhost:2181 --topic "fasten.$topic.out"
+  kafka-topics.sh --create --zookeeper localhost:2181 --topic "fasten.$topic.err"
 done
 
 kafka-topics.sh --create --zookeeper localhost:2181 --topic fasten.RapidPlugin.log
+
+kafka-consumer-groups.sh --bootstrap-server localhost:9092 --delete --group eu.fasten.analyzer.pomanalyzer.POMAnalyzerPlugin.POMAnalyzer
+kafka-consumer-groups.sh --bootstrap-server localhost:9092 --delete --group eu.fasten.analyzer.javacgopal.OPALPlugin.OPAL
+kafka-consumer-groups.sh --bootstrap-server localhost:9092 --delete --group eu.fasten.analyzer.repoclonerplugin.RepoClonerPlugin.RepoCloner
+kafka-consumer-groups.sh --bootstrap-server localhost:9092 --delete --group eu.fasten.analyzer.metadataplugin.MetadataDatabasePlugin.MetadataDBExtension
+kafka-consumer-groups.sh --bootstrap-server localhost:9092 --delete --group eu.fasten.analyzer.graphplugin.GraphDatabasePlugin.GraphDBExtension
+kafka-consumer-groups.sh --bootstrap-server localhost:9092 --delete --group fasten.pycg
+
